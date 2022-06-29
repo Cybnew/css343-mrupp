43e79de105e4f066b4a5547026cab46e742a0a6c
Mathew Rupp <mathewrupp@gmail.com>
2022-06-28 18:05:53 -0700

    Final Commit

--- HW1/source/hw1.cpp
+++ HW1/source/hw1.cpp
@@ -6,6 +6,8 @@
 #include <string>
 using namespace std;
 
+
+//A Helper Function for displaying what shuffle we are on
 void uiHelper(int i){
   map<int, string> shuffleOutputText {{ 1, "first"}, { 2, "second" }, { 3, "third" }, { 4, "fourth" }, { 5, "fifth" }, { 6, "sixth" }, { 7, "seventh" }, { 8, "eighth" }, { 9, "ninth" }, { 10, "tenth" }};
 cout << "The " << shuffleOutputText[i] << " shuffled output is: " << endl;
@@ -14,6 +16,8 @@ int randomSeed(int i){
   return rand() % i;
 }
 
+
+//Prints the 3x3 grid
 void printShuffel(char arr[]){
 
   cout << "+-------+" << endl;
@@ -23,18 +27,21 @@ void printShuffel(char arr[]){
   cout << "+-------+" << endl;
 }
 
+//Shuffles the number array
+
 void shuffelArray(char arr[]){
   randomSeed(unsigned(time(0)));
   random_shuffle(arr,arr+9,randomSeed);
   printShuffel(arr);
 }
 
+//Reads the input from the user
 void getInput(char *arr){
   cout << "Please input 9 digits to be randomly shuffled:" << endl;
   cin >> arr;
 
 }
-
+//Main Program
 int main(int argc, char* argv[]){
 
 char* a = argv[1];
